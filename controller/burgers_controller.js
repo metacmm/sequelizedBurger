e237dd2140e5bb8aa8ac11debdee609020920a5c
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2019-12-08 22:29:09 -0800

    complete render index after joining burger and customer.

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -3,68 +3,92 @@ var db = require("../models");
 var router = express.Router();
 var Sequelize = require("sequelize");
 
-router.get("/", function(req, res){
+router.get("/", function (req, res) {
 
     db.Burger.findAll({
-        include:[db.Customer],
-    }).then(function(data1){
-        db.Burger.findAll({
-            where:{
-                quantity:{
-                    [Sequelize.Op.gt]: 0
-                } 
-            }
-        }).then(function(data2){
-            var hbsObject = {
-                burgers:data1,
-                customers: data1.Customers,
-                availableBurgers: data2
+        include: [db.Customer],
+    }).then(function (data) {
+        console.log(data);
+        var hbsObject = {
+            burgers: [],
+            availableBurgers: [],
+        };
+
+        for(var i = 0; i < data.length; i++){
+            var arrServedCustomer = [];
+            var newBurger = {
+                id: data[i].id,
+                name: data[i].name,
+                customers:[],
+                arrServedCustomer:[]
             };
-            res.render("index", hbsObject);
-        });
+            if (data[i].quantity > 0){
+                hbsObject.availableBurgers.push(
+                    {
+                        id:data[i].id,
+                        name:data[i].name,
+                    });
+            }
+            console.log(data[i].Customers);
+
+            for (var j = 0; j < data[i].Customers.length; j++){
+                if (data[i].Customers[j].served){
+                    newBurger.arrServedCustomer.push(data[i].Customers[j].name);
+                } else {
+                    newBurger.customers.push({
+                        customerId: data[i].Customers[j].id,
+                        customerName: data[i].Customers[j].name
+                    });
+                }
+            }
+        
+
+            hbsObject.burgers.push(newBurger);
+        }
+        res.render("index", hbsObject);
     });
 });
 
-router.put("/api/burgers/:id", function(req, res){
+router.put("/api/burgers/:id", function (req, res) {
     db.Burger.update(
         {
             name: req.body.name,
             devoured: req.body.devoured
-        },{
-        where:{
-            id: req.params.id
-        }
-    }).then(function(data){
-        var hbsObject = {
-            burgers: data
-        }
-        res.render("index",hbsObject);
-    });
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function (data) {
+            var hbsObject = {
+                burgers: data
+            }
+            res.render("index", hbsObject);
+        });
 });
 
-router.post("/api/burgers", function(req, res){
+router.post("/api/burgers", function (req, res) {
     console.log(req.body);
     db.Burger.create(
         {
             name: req.body.name,
             quantity: req.body.quantity
         }
-    ).then(function(data){
+    ).then(function (data) {
         var hbsObject = {
             burgers: data
         }
-        res.json({id: data.id});
+        res.json({ id: data.id });
     })
 });
 
-router.post("/api/customers", function(req, res){
+router.post("/api/customers", function (req, res) {
     db.Customer.create(
         {
             name: req.body.name,
             BurgerId: req.body.BurgerId
         }
-    ).then(function(data){
-        res.json({id: data.id});
+    ).then(function (data) {
+        res.json({ id: data.id });
     })
 })
 module.exports = router;
